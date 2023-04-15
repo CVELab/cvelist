51fe0f39b02db55820de09fc393e15aa253b36c9
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-04-15 12:33:11 -0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,35 +1,28 @@
-import os
-import sys
-from github import Github
+import requests
 
-# Get GitHub API token from environment variable
-api_token = os.environ.get("GITHUB_TOKEN")
 
-if not api_token:
-    sys.exit("Error: Missing GITHUB_TOKEN environment variable")
+def cve_to_md(cve_json):
+    id = cve_json["id"]
+    url = cve_json["url"]
+    product = cve_json["product"]
+    version = cve_json["version"]
+    vulnerability = cve_json["vulnerability"]
+    description = cve_json["description"]
 
-# Authenticate with the GitHub API
-g = Github(api_token)
+    md = f"### [{id}]({url})\n"
+    md += "![](https://img.shields.io/static/v1?label=Product&message=n%2Fa&color=blue)\n"
+    md += "![](https://img.shields.io/static/v1?label=Version&message=n%2Fa&color=blue)\n"
+    md += "![](https://img.shields.io/static/v1?label=Vulnerability&message=n%2Fa&color=brighgreen)\n\n"
+    md += f"### Description\n\n{description}\n\n"
+    
+    if len(cve_json["poc"]) > 0:
+        md += "### POC\n\n"
+        for poc in cve_json["poc"]:
+            md += f"#### {poc['description']}\n\n- {poc['url']}\n\n"
 
-# Get all advisories
-advisories = g.get_advisories()
+    if len(cve_json["github"]) > 0:
+        md += "### Github\n\n"
+        for gh in cve_json["github"]:
+            md += f"#### {gh['title']}\n\n- {gh['url']}\n\n"
 
-# Convert advisories to Markdown format
-for advisory in advisories:
-    cve_id = advisory.identifiers.get("CVE", {}).get("id", "N/A")
-    product = advisory.affected_package_ranges[0].package_name if advisory.affected_package_ranges else "N/A"
-    description = advisory.summary
-    pocs = advisory.references.get("poc", [])
-    pocs_md = "\n".join([f"- {poc}" for poc in pocs])
-    github_link = advisory.html_url
-
-    # Write output to STDOUT
-    print(f"### [{cve_id}]({github_link})")
-    print(f"![](https://img.shields.io/static/v1?label=Product&message={product}&color=blue)")
-    print("![](https://img.shields.io/static/v1?label=Version&message=n%2Fa&color=blue)")
-    print("![](https://img.shields.io/static/v1?label=Vulnerability&message=n%2Fa&color=brighgreen)\n")
-    print(f"### Description\n\n{description}\n")
-    if pocs_md:
-        print("### POC\n")
-        print(f"#### Reference\n{pocs_md}\n")
-    print(f"#### GitHub\n{github_link}\n")
+    return md
